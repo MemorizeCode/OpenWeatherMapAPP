cf8f0b8bf398c290d0e59baf0715ac31040a4446
MemorizeCode <ilya.bezrukov.2005@mail.ru>
2025-08-09 16:14:46 +0400

    fix init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 
 const tg = window.Telegram.WebApp 
@@ -15,6 +15,7 @@ function App() {
 
   return (
     <div>
+      <h2>Tg app</h2>
       <button onClick={onClose}>Close App</button>
     </div>
   )
