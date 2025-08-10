ab48fa0f06d8bf153d1886f6cf0d935ea901a8a5
MemorizeCode <ilya.bezrukov.2005@mail.ru>
2025-08-10 14:21:18 +0400

    fix css nav

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import './Nav.css'
 
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/canvas" element={<Canvas />} />
-      </Routes>
-      <nav>
+    <div className="app-container">
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/canvas" element={<Canvas />} />
+        </Routes>
+      </div>
+      <nav className="bottom-nav">
         <Link to={'/'}>Clicker</Link>
         <Link to={'/canvas'}>Canvas</Link>
       </nav>
-    </>
+    </div>
   )
 }
 
